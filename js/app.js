3c4d5fc0c43562e154259b1e93293fca30ecdee5
Printbam <printbam.express@gmail.com>
2026-02-17 00:47:32 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4534,3 +4534,10 @@ async function chargerPosts() {
         }
     }
 }
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('./sw.js')
+            .then(reg => console.log('✅ Service Worker enregistré', reg))
+            .catch(err => console.error('❌ Erreur SW', err));
+    });
+}
